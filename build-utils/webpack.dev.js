0ed4ff82c24ec3217ef0928cd32d905e6cadd9d4
Saludo <alexis.j.saludo@hotmail.fr>
2021-09-28 01:05:38 +0200

    Fix not necessary import and old property

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,18 +1,15 @@
 const paths = require("./paths");
-const webpack = require("webpack");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new Dotenv({
       path: paths.root + "/.env.development"
     })
   ],
   devServer: {
     historyApiFallback: true,
-    contentBase: paths.build,
     open: true,
     compress: true,
     hot: true,
