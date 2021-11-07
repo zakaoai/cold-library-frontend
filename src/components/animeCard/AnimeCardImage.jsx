80d583a3920cf4bfc62d31ac819b558e32727672
Saludo <alexis.j.saludo@hotmail.fr>
2021-11-07 15:02:56 +0100

    Open in new tab MAL

--- src/components/animeCard/AnimeCardImage.jsx
+++ src/components/animeCard/AnimeCardImage.jsx
@@ -5,7 +5,7 @@ import { Skeleton } from "@mui/material";
 function AnimeCardImage({ url, imageUrl, title, imageHeight = "190px" }) {
   return (
     (imageUrl && (
-      <a href={url}>
+      <a href={url} target="_blank" rel="noreferrer">
         <CardMedia component={"img"} style={{ maxHeight: imageHeight }} src={imageUrl} title={title} />
       </a>
     )) || <Skeleton animation="wave" variant="rectangular" height={190} />
