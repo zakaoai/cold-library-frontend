9ef1bbe79721d1e07fdfd331be7cb13e2c36f679
Alexis SALUDO <alexis.j.saludo@hotmail.fr>
2023-10-19 16:47:45 +0200

    Ajout d'un footer

---
+++ src/containers/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+const Footer = () => (
+  <Box
+    sx={{
+      backgroundColor: theme => (theme.palette.mode === "light" ? theme.palette.grey[200] : theme.palette.grey[800]),
+      p: 1,
+      mt: "auto"
+    }}
+    component="footer">
+    <Container maxWidth="sm">
+      <Typography variant="body2" color="text.secondary" align="center">
+        {"Copyright © "}
+        Cold Library App V{APP_VERSION} - {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    </Container>
+  </Box>
+);
+
+export default Footer;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
+declare const APP_VERSION: string;
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_APP_VERSION: string;
   // more env variables...
 }
 
