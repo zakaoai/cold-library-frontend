66339ca307b558765d35267330140d7e4721d3e2
Alexis SALUDO <alexis.j.saludo@hotmail.fr>
2024-02-13 18:33:06 +0100

    TS

--- src/containers/Activite/AnimeEpisode/AnimeEpisodeBar.tsx
+++ src/containers/Activite/AnimeEpisode/AnimeEpisodeBar.tsx
@@ -1,13 +1,14 @@
+import type IAnimeEpisodeBar from "@/interfaces/containers/Activite/AnimeEpisode/AnimeEpisodeBar"
 import SearchIcon from "@mui/icons-material/Search"
 import { AppBar, Box, Button, Grid } from "@mui/material"
 
-const AnimeEpisodeBar = ({ update }) => {
+const AnimeEpisodeBar = ({ update }: IAnimeEpisodeBar) => {
   return (
     <Box mb={1}>
       <AppBar position="relative" color="transparent">
         <Grid container alignItems="center">
           <Grid item>
-            <Button variant="outlined" onClick={() => update()} startIcon={<SearchIcon />}>
+            <Button variant="outlined" onClick={update} startIcon={<SearchIcon />}>
               Update Anime Infos
             </Button>
           </Grid>
--- src/containers/Activite/AnimeEpisode/EpisodeLine.tsx
+++ src/containers/Activite/AnimeEpisode/EpisodeLine.tsx
@@ -1,17 +1,16 @@
+import type IEpisodeLine from "@/interfaces/containers/Activite/AnimeEpisode/EpisodeLine"
 import TableCell from "@mui/material/TableCell"
 import TableRow from "@mui/material/TableRow"
 import { DateTime } from "luxon"
 
-const EpisodeLine = ({ episode }) => {
-  const { episodeNumber, title, dateSortie } = episode
+const EpisodeLine = ({ episode }: IEpisodeLine) => {
+  const { episodeNumber, title, date } = episode
 
   return (
     <TableRow hover key={episodeNumber}>
       <TableCell align="center">{episodeNumber}</TableCell>
       <TableCell align="left">{title}</TableCell>
-      <TableCell align="left">
-        {dateSortie && DateTime.fromSeconds(dateSortie).setLocale("fr").toFormat("dd LLL yyyy")}
-      </TableCell>
+      <TableCell align="left">{date && DateTime.fromSeconds(date).setLocale("fr").toFormat("dd LLL yyyy")}</TableCell>
     </TableRow>
   )
 }
