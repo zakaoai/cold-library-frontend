f717129b8a8316e5e8e8914f44ce1a986515a71e
Saludo <alexis.j.saludo@hotmail.fr>
2024-04-05 18:33:18 +0200

    Lazy Load image

--- src/components/MALCard/MALCardImage.tsx
+++ src/components/MALCard/MALCardImage.tsx
@@ -1,10 +1,11 @@
 import CardMedia from "@mui/material/CardMedia"
 import Skeleton from "@mui/material/Skeleton"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 import { MALCardProps } from "./MALCard"
 
 const MALCardImage = ({ malAnime }: MALCardProps) => {
   const {
-    main_picture: { large: malImg },
+    main_picture: { medium: malImg },
     title,
     id
   } = malAnime
@@ -12,7 +13,9 @@ const MALCardImage = ({ malAnime }: MALCardProps) => {
 
   return malImg ? (
     <a href={malUrl} target="_blank" rel="noreferrer">
-      <CardMedia component={"img"} src={malImg} title={title} />
+      <CardMedia>
+        <LazyLoadImage src={malImg} title={title} style={{ width: "100%" }} />
+      </CardMedia>
     </a>
   ) : (
     <Skeleton animation="wave" variant="rectangular" height={190} />
