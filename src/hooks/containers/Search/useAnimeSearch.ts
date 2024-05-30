4c457a87a9a9210c0ad23765b446d34ef4da71a1
Saludo <alexis.j.saludo@hotmail.fr>
2024-05-30 22:54:45 +0200

    add page to search activity

--- src/hooks/containers/Search/useAnimeSearch.ts
+++ src/hooks/containers/Search/useAnimeSearch.ts
@@ -1,37 +1,48 @@
 import type SearchFormValues from "@/interfaces/containers/Activite/Search/SearchFormValues"
 import { type AnimeDTO } from "@/interfaces/services/AnimeService/AnimeDTO"
-import SiteMap from "@/routes/SiteMap"
+import type DataListHolderWithPagination from "@/interfaces/services/DataListHolderWithPagination"
 import AnimeServices from "@/services/AnimeService"
 import { useMutation } from "@tanstack/react-query"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useState, type ChangeEvent as ReactChangeEvent } from "react"
 import { useForm } from "react-hook-form"
-import { useLocation, useNavigate } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
 import useLibrary from "../AnimeLibrary/useLibrary"
 
 const useAnimeSearch = () => {
   useLibrary()
-  const { search: browserSearch } = useLocation()
-  const searchParam = new URLSearchParams(browserSearch).get("search")
 
-  const form = useForm<SearchFormValues, unknown>({ defaultValues: { search: searchParam ?? "" } })
+  const [searchParams, setSearchParams] = useSearchParams()
+  const page = searchParams.get("page")
+  const [pageMax, setPageMax] = useState<number | undefined>(undefined)
+  const [currentPage, setCurrentPage] = useState<number | undefined>(page !== null ? +page : undefined)
 
-  const navigate = useNavigate()
+  const form = useForm<SearchFormValues, unknown>({ defaultValues: { search: searchParams.get("search") ?? "" } })
 
   const [animes, setAnimes] = useState<AnimeDTO[]>([])
 
   const [error, setError] = useState<string | undefined>(undefined)
 
   const searchAnimeCall = useCallback(
-    async (search: string) => {
-      navigate({ pathname: SiteMap.RECHERCHE.path, search: `?search=${search}` })
-      return await AnimeServices.searchAnime(search)
+    async ({ search, page }: { search: string; page: number | undefined }) => {
+      // navigate({ pathname: SiteMap.RECHERCHE.path, search: `?search=${search}` })
+      setSearchParams(prev => {
+        prev.set("search", search)
+        console.log(page)
+        if (page !== undefined) prev.set("page", page?.toString())
+        else prev.delete("page")
+        return prev
+      })
+      return await AnimeServices.searchAnime(search, page)
     },
-    [navigate]
+    [setSearchParams]
   )
 
   const onSuccessSearchAnime = useCallback(
-    (animes: AnimeDTO[]) => {
-      setAnimes(animes)
+    (result: DataListHolderWithPagination<AnimeDTO>) => {
+      console.log(result.pagination)
+      setAnimes(result.data)
+      setPageMax(result.pagination.last_visible_page)
+      setCurrentPage(result.pagination.current_page)
     },
     [setAnimes]
   )
@@ -39,15 +50,21 @@ const useAnimeSearch = () => {
     setError("Une erreur est survenue lors de la recherche")
   }, [setError])
 
-  const { isPending, mutate: searchAnime } = useMutation<AnimeDTO[], unknown, string>({
+  const { isPending, mutate: searchAnime } = useMutation<
+    DataListHolderWithPagination<AnimeDTO>,
+    unknown,
+    { search: string; page: number | undefined }
+  >({
     mutationFn: searchAnimeCall,
     onSuccess: onSuccessSearchAnime,
     onError: onErrorSearchAnime
   })
 
   useEffect(() => {
-    if (searchParam !== null) {
-      searchAnime(searchParam)
+    const search = searchParams.get("search")
+    if (search !== null) {
+      console.log("INITSEARCH")
+      searchAnime({ search, page: currentPage })
     }
   }, [])
 
@@ -57,7 +74,14 @@ const useAnimeSearch = () => {
     )
   }
 
-  return { animes, error, isFetching: isPending, form, searchAnime, updateAnime }
+  const handleChange = useCallback(
+    (_event: ReactChangeEvent<unknown>, page: number) => {
+      searchAnime({ search: form.getValues("search"), page })
+    },
+    [form, searchAnime]
+  )
+
+  return { animes, error, isFetching: isPending, form, searchAnime, updateAnime, pageMax, currentPage, handleChange }
 }
 
 export default useAnimeSearch
