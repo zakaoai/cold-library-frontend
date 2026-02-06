c4404555239260bc4468aec70d9add8a1728a138
Zakaoai <alexis.j.saludo@hotmail.fr>
2026-02-06 14:14:48 +0100

    ✨ Corrige le formatage des dates
    Corrige le formatage des dates en provenance de Java.
    
    Utilise maintenant une string standard au lieu d'un tableau de nombres.
    Cela simplifie le code et améliore la lisibilité.

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,15 +1,9 @@
 import { format } from "date-fns/format"
 
-export const formatJavaLocalDateTimeArray = (date: [number, number, number, number, number, number, number]) => {
-  const copiedDate = [...date] as [number, number, number, number, number, number, number]
-  copiedDate[6] = 0
-  copiedDate[1]--
-  return Array.isArray(copiedDate) && format(new Date(Date.UTC(...copiedDate)), "dd/MM/yyyy HH:mm:ss")
+export const formatJavaLocalDateTimeArray = (date: string) => {
+  return format(new Date(date), "dd/MM/yyyy HH:mm:ss")
 }
 
-export const formatJaveLocalDateArray = (date: [number, number, number, number, number]) => {
-  const copiedDate = [...date] as [number, number, number, number, number]
-
-  copiedDate[1]--
-  return Array.isArray(copiedDate) && format(new Date(Date.UTC(...copiedDate)), "dd/MM/yyyy")
+export const formatJaveLocalDateArray = (date: string) => {
+  return format(new Date(date), "dd/MM/yyyy")
 }
