9e0247846a8b21fb9084d4d67ee550fd03b0031a
Zakaoai <alexis.j.saludo@hotmail.fr>
2026-06-27 23:53:51 +0200

    fix linter

--- eslint.config.js
+++ eslint.config.js
@@ -8,10 +8,11 @@ import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
+import { defineConfig } from "eslint/config"
 import globals from "globals"
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+export default defineConfig([
   ...pluginQuery.configs["flat/recommended"].map(a => ({ name: "config-query", ...a })),
   pluginPromise.configs["flat/recommended"],
 
@@ -57,7 +58,8 @@ export default [
         Object.entries(love.plugins).filter(
           ([key]) =>
             ![
-              "promise"
+              "promise",
+              "import"
               //  "@typescript-eslint"
             ].includes(key)
         )
@@ -103,5 +105,6 @@ export default [
       "@typescript-eslint/no-magic-numbers": "warn",
       "no-console": "off"
     }
-  }
-]
+  },
+  { settings: { react: { version: "19" } } }
+])
